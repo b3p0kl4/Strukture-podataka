72155fe6361d637705160fd872a580c0b16cbc26
b3p0kl4 <bepo.klarin@gmail.com>
2025-10-07 21:07:50 +0200

    zadatak 1

---
+++ lab1/lab1/lab1/lab1.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+typedef struct student {
+	char ime[15] = {""};
+	char prezime[15] = {""};
+	int apsBodovi = 0;
+	int relBodovi = 0;
+}Stud;
+
+int main() {
+	brojRedaka;					//prva funckija, brojanje redaka
+	dinAlokacija;				//druga funckija, alociranje memorije za niz struktura
+	ispisPodataka;				//treca funckija, ispis podataka
+	relBrojBodova;				//cetvrta funkcija, racunanje relativnog broja bodova
+	return 0;
+
+}
